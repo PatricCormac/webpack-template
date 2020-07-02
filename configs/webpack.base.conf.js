b05b6282fc87619a692a066fa82fe416d0de9efd
PatricCormac <socolov1897@gmail.com>
2020-07-03 04:16:50 +0500

    webpack bundle

---
+++ configs/webpack.base.conf.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const MiniCssExtarctPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+};
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_module/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtarctPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {sourceMap: true}
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: `${PATHS.src}/js/postcss.config.js`
+              }
+            }
+          }
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          MiniCssExtarctPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {sourceMap: true}
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: `${PATHS.src}/js/postcss.config.js`
+              }
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {sourceMap: true}
+          }
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtarctPlugin({
+      filename: `${PATHS.assets}css/[name].[hash].css`
+    }),
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html',
+      //inject: false
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+        { from: `${PATHS.src}/static`, to: '' }
+      ],
+    }),
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './js/common.js';
+import './style/main.less';
