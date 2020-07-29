d5284c1ceb3e9358623433869718a457c368d8af
PatricCormac <socolov1897@gmail.com>
2020-07-29 16:56:11 +0500

    Переписываю шаблон,поддержка шрифтов, стилей, изображений

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import './js/common.js';
-import './assets/style/main.less';
+import Post from "./Post";
+import json from "./assets/title";
+import "./styles/styles.css";
+import logo from "./assets/logo.png";
+
+const post = new Post("Webpack Title", logo);
+
+console.log("Post to string: ", post.toString());
+
+console.log("JSON:", json);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./index.js",
+    analytics: "./analytics.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+};
